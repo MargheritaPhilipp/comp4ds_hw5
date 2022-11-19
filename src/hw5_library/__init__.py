c639aeff1074346a97685553c87db11b3f20c1f4
Margherita-IDEMS <91319540+MargheritaPhilipp@users.noreply.github.com>
2022-11-19 17:47:43 +0100

    :cactus: renaming and deleting
    removed old function files and started editing innit files

---
+++ src/hw5_library/__init__.py
@@ -0,0 +1,7 @@
+__all__=['data_functions', 'hw5_functions']
+
+from .data_functions import
+
+
+from .hw5_functions.dictionaries import retrieve_age_eafp2
+from .hw5_functions.dictionaries import retrieve_age_lbyl
